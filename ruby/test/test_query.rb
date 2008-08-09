3cd2ae22dc897f5309da72a97cc47351214a5ac0
makoto kuwata <kwa@kuwata-lab.com>
2008-08-09 11:48:17 +0900

    Add many tests.

--- ruby/test/test_query.rb
+++ ruby/test/test_query.rb
@@ -5,23 +5,21 @@
 ### $License$
 ###
 
-p File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'lib')
-$:.unshift File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'lib')
+File.class_eval do
+  path = join(dirname(dirname(expand_path(__FILE__))), 'lib')
+  $:.unshift path
+  path = join(dirname(dirname(expand_path(__FILE__))), 'test')
+  $:.unshift path
+end
 
 
 #require 'rubygems'
 #require 'rspec'
 require 'kwery'
-require 'mysql'
+require 'database_config'
 
 
-HOST = 'localhost'
-USER = 'user1'
-PASS = 'passwd1'
-DBNAME = 'example1'
-
-
-conn = Mysql.connect(HOST, USER, PASS, DBNAME)
+conn = DB_CONNECTION
 q = Kwery::Query.new(conn)
 #q.debug = true
 now = :current_timestamp
@@ -29,8 +27,10 @@ now = :current_timestamp
 
 describe 'Kwery::Query#execute' do
 
-  it "should execute create table sql statement." do
-    q.execute "drop table if exists teams"
+  create_table_option = ' engine=InnoDB'
+  drop_table_option = ' if exists'
+  it "executes create table sql statement" do
+    q.execute "drop table #{drop_table_option} teams"
     q.execute <<END
 create table teams (
   id         integer       primary key auto_increment,
@@ -39,9 +39,9 @@ create table teams (
   owner_id   integer       references members(id),
   created_at timestamp     not null,
   updated_at timestamp     not null
-)
+) #{create_table_option}
 END
-    q.execute "drop table if exists members"
+    q.execute "drop table #{drop_table_option} members"
     q.execute <<END
 create table members (
   id         integer       primary key auto_increment,
@@ -50,9 +50,8 @@ create table members (
   team_id    integer       references teams(id),
   created_at timestamp     not null,
   updated_at timestamp     not null
-)
+) #{create_table_option}
 END
-
   end
 
 end
@@ -60,7 +59,7 @@ end
 
 describe 'Kwery::Query#insert' do
 
-  it "is able to insert hash data" do
+  it "inserts hash data" do
     q.insert('teams', {:name=>'sos', :desc=>'SOS Brigate'})
     id = q.last_insert_id
     #
@@ -72,7 +71,7 @@ describe 'Kwery::Query#insert' do
     q.select('members').length.should == 3
   end
 
-  it "is able to insert array data" do
+  it "inserts array data" do
     q.insert('teams', [nil, 'ryouou', 'Ryouou Gakuen High School', nil, now, now])
     id = q.last_insert_id
     #
@@ -90,14 +89,14 @@ end
 
 describe 'Kwery::Query#update' do
 
-  it "is able to update data with conditions" do
+  it "updates data with conditions" do
     desc = "Haruhi's brigate"  # contains "'"
     q.update('teams', {:desc=>desc, :updated_at=>Time.now}) {|c| c.where('name = ', 'sos') }
     hash = q.get('teams', 1)
     hash['desc'].should == desc
   end
 
-  it "is able to update data with id" do
+  it "updates data with id" do
     mikuru = q.get('members') {|c| c.where(:name, 'Mikuru') }
     mikuru['desc'].should == nil
     desc = 'Future-woman'
@@ -106,16 +105,246 @@ describe 'Kwery::Query#update' do
     mikuru['desc'].should == desc
   end
 
-  it "should throw ArugmentError when condition is not specified" do
-    ex = nil
+  it "throws ArugmentError when condition is not specified" do
+    msg = 'update condition is reqiured.'
+    proc { q.update('teams', {:owner_id=>1}) }.should raise_error(ArgumentError, msg)
+  end
+
+end
+
+
+describe 'Kwery::Query#get' do
+
+  it "returns a Hash object" do
+    hash = q.get('members', 1)
+    hash.should be_kind_of(Hash)
+    hash['id'].to_i.should == 1
+    hash['name'].should == 'Haruhi'
+  end
+
+  it "can take a block" do
+    hash = q.get('members') {|c| c.where(:id, 1) }
+    hash.should be_kind_of(Hash)
+    hash['id'].to_i.should == 1
+    hash['name'].should == 'Haruhi'
+  end
+
+  it "returns nil when data not found" do
+    hash = q.get('members', 999)
+    hash.should == nil
+    hash = q.get('members') {|c| c.where(:name, 'Minoru') }
+    hash.should == nil
+  end
+
+end
+
+
+describe 'Kwery::Query#get_all' do
+
+  it "returns an Array of Hash object" do
+    list = q.get_all('members')
+    list.should be_a_kind_of(Array)
+    list.length.should == 7
+    list.first.should be_a_kind_of(Hash)
+  end
+
+  it "can take a block" do
+    list = q.get_all('members') {|c| c.where(:team_id, 1) }
+    list.should be_a_kind_of(Array)
+    list.first.should be_a_kind_of(Hash)
+    list.length.should == 3
+    list.each {|hash| hash['team_id'].to_i.should == 1 }
+  end
+
+  it "returns empty Array when data not found" do
+    list = q.get_all('members') {|c| c.where(:team_id, 999) }
+    list.should be_a_kind_of(Array)
+    list.length.should == 0
+  end
+
+end
+
+
+describe 'Kwery::Query#select' do
+
+  it "returns enumerable" do
+    enum = q.select('members')
+    enum.should be_a_kind_of(Enumerable)
+    arr = enum.to_a
+    arr.length.should == 7
+  end
+
+  it "can take a block" do
+    enum = q.select('members') {|c| c.where(:team_id, 1) }
+    arr = enum.to_a
+    arr.collect {|e| e['name'] }.should == ['Haruhi', 'Mikuru', 'Yuki']
+  end
+
+  it "can take column names" do
+    enum = q.select('members', 'id, name') {|c| c.where(:team_id, 1) }
+    arr = enum.to_a
+    arr.each {|h| h['id'] = h['id'].to_i }
+    arr.should == [{'id'=>1, 'name'=>'Haruhi'}, {'id'=>2, 'name'=>'Mikuru'}, {'id'=>3, 'name'=>'Yuki'}]
+  end
+
+  class Member1
+    attr_accessor :id, :name, :desc, :team_id, :created_at, :updated_at, :deleted
+  end
+
+  it "can take class object" do
+    enum = q.select('members', nil, Hash) {|c| c.where(:team_id, 1) }
+    enum.to_a[0].should be_a_kind_of(Hash)
+    enum = q.select('members', nil, Array) {|c| c.where(:team_id, 1) }
+    enum.to_a[0].should be_a_kind_of(Array)
+    enum.to_a[0][1].should == 'Haruhi'
+    enum = q.select('members', nil, Member1) {|c| c.where(:team_id, 1) }
+    enum.to_a[0].should be_a_kind_of(Member1)
+    yuki = enum.to_a[2]
+    yuki.name.should == 'Yuki'
+    yuki.team_id.to_i.should == 1
+  end
+
+  expected = <<END
+1, Haruhi, 1, sos
+2, Mikuru, 1, sos
+3, Yuki, 1, sos
+4, Konata, 2, ryouou
+5, Kagami, 2, ryouou
+6, Tsukasa, 2, ryouou
+7, Miyuki, 2, ryouou
+END
+
+  it "can take several table names" do
+    columns = 'members.*, teams.name team_name'
+    enum = q.select('members, teams', columns) {|c| c.where('members.team_id = teams.id').order_by('members.id') }
+    arr = enum.to_a
+    #collected = arr.collect {|e| [e['id'], e['name'], e['team_id'], e['team_name']].join(", ") }
+    collected = arr.collect {|e| e.values_at(*%w[id name team_id team_name]).join(", ") }
+    result = collected.join("\n") << "\n"
+    result.should == expected
+  end
+
+  it "supports table name aliases" do
+    columns = 'm.*, t.name team_name'
+    enum = q.select('members m, teams t', columns) {|c| c.where('m.team_id = t.id').order_by('m.id') }
+    arr = enum.to_a
+    #collected = arr.collect {|e| [e['id'], e['name'], e['team_id'], e['team_name']].join(", ") }
+    collected = arr.collect {|e| e.values_at(*%w[id name team_id team_name]).join(", ") }
+    result = collected.join("\n") << "\n"
+    result.should == expected
+  end
+
+  it "supports join" do
+    haruhi_id = 1
+    q.update('teams', {:owner_id=>haruhi_id}) {|c| c.where(:name, 'sos') }
+    q.update('teams', {:owner_id=>nil}) {|c| c.where(:name, 'ryouou') }
+    #
+    enum = q.select('teams, members') {|c| c.where('teams.owner_id = members.id').order_by('teams.id') }
+    arr = enum.to_a
+    arr.length.should == 1
+    #
+    columns = 'teams.*, members.name owner_name'
+    enum = q.select('teams', columns) {|c| c.left_outer_join('members', :owner_id).order_by('teams.id') }
+    arr = enum.to_a
+    arr.length.should == 2
+    collected = arr.collect {|e| e.values_at(*%w[id name owner_id owner_name]).join(', ') }
+    actual = collected.join("\n") << "\n"
+    expected = <<END
+1, sos, 1, Haruhi
+2, ryouou, , 
+END
+    actual.should == expected
+  end
+
+  it "supports join with specifying class object" do
+    columns = 'teams.*, members.name owner_name'
+    enum = q.select('teams', columns, Array) {|c| c.left_outer_join('members', :owner_id).order_by('teams.id') }
+    arr = enum.to_a
+    arr.length.should == 2
+    arr[0].should be_a_kind_of(Array)
+  end
+
+end
+
+
+describe 'Kwery::Query#transaction' do
+
+  it "commits when no errors" do
+    kagami = q.get('members') {|c| c.where(:name, 'Kagami') }
+    id = kagami['id']
+    s = 'Hiiragi Kagami'
+    q.transaction do
+      q.update('members', {:desc=>s}, id)
+    end
+    q.get('members', id)['desc'].should == s
+  end
+
+  it "rollbacks when error raised" do
+    tsukasa = q.get('members') {|c| c.where(:name, 'Tsukasa') }
+    id = tsukasa['id']
+    s = 'Yo-ni-ge de reset'
     proc {
-      begin
-        q.update('teams', {:owner_id=>1})
-      rescue Exception => ex
-        raise ex
+      q.transaction do
+        proc {
+          q.update('members', {:desc=>s}, id)
+          q.get('members', id)['desc'].should == s
+        }.should_not raise_error(Exception)
+        q.update('members', {:description=>s}, id)  # error
       end
-    }.should raise_error(ArgumentError)
-    ex.message.should == 'update condition is reqiured.'
+    }.should raise_error(DB_ERROR_CLASS)  # Unknown column 'description' in 'field list'
+    q.get('members', id)['desc'].should_not == s
+    q.get('members', id)['desc'].should == tsukasa['desc']
+  end
+
+end
+
+
+describe 'Kwery::Query#delete' do
+
+  it "deletes data specified by id" do
+    #q.debug = true
+    miyuki = q.get('members') {|c| c.where('name', 'Miyuki') }
+    q.delete('members', miyuki['id'])
+    q.get('members') {|c| c.where('name', 'Miyuki') }.should == nil
+    q.get_all('members').length.should == 6
+  end
+
+  it "deletes data specified by block" do
+    #q.debug = true
+    ryouou = q.get('teams') {|c| c.where('name', 'ryouou') }
+    id = ryouou['id']
+    q.delete('members') {|c| c.where('team_id', id) }
+    q.get_all('members').length.should == 3
+    q.get_all('members') {|c| c.where('team_id', id) }.length.should == 0
+  end
+
+  it "throws ArugmentError when condition is not specified" do
+    msg = 'delete condition is reqiured.'
+    proc { q.delete('teams') }.should raise_error(ArgumentError, msg)
+  end
+
+end
+
+
+describe "Kwery::Qwery#delete_all" do
+
+  it "deletes all data" do
+    q.delete_all('members')
+    q.get_all('members').length.should == 0
+  end
+
+end
+
+
+describe "Kwery::Qwery#last_insert_id" do
+
+  it "returns last insert id" do
+    q.insert('members', :name=>'Yutaka')
+    id = q.last_insert_id
+    id.should be_a_kind_of(Fixnum)
+    #id.should == 7
+    q.insert('members', :name=>'Minami')
+    q.last_insert_id.should == (id+1)
   end
 
 end
