ebe115060d4f9e02263a203004a8ad767e625e47
makoto kuwata <kwa@kuwata-lab.com>
2008-08-25 23:22:28 +0900

    Enhanced to define new module MetaModel and move eval code from Model.add_columns() to it.

--- ruby/lib/kwery/model.rb
+++ ruby/lib/kwery/model.rb
@@ -9,7 +9,96 @@
 module Kwery
 
 
+  module MetaModel
+
+    attr_accessor :__table__
+    attr_reader :__column_names__
+    attr_reader :__columns__, :create_table_sql
+
+    def set_table_name(table_name)
+      @__table__ = table_name
+    end
+
+    def _define_accessors(column_names)   # :nodoc:
+      attr_accessor *column_names
+      buf = ''
+      column_names.each do |col|
+        buf << <<-END
+          def #{col}=(val)
+            @__old__[:#{col}] = val if @__old__ && @#{col} != val
+            @#{col} = val
+          end
+        END
+      end
+      self.class_eval(buf)
+    end
+
+    def _define_hooks(column_names)  # :nodoc:
+      has_created_at = column_names.include?(:created_at)
+      has_updated_at = column_names.include?(:updated_at)
+      if has_created_at && has_updated_at
+        self.class_eval do
+            def __before_insert__(values)
+              values[:created_at] = @created_at = __current_timestamp__()
+              values[:updated_at] = @updated_at = __current_timestamp__()
+            end
+        end
+      elsif has_created_at
+        self.class_eval do
+            def __before_insert__(values)
+              values[:created_at] = @created_at = __current_timestamp__()
+            end
+        end
+      end
+      if has_updated_at
+        self.class_eval do
+            def __before_update__(values)
+              values[:updated_at] = @updated_at = __current_timestamp__()
+            end
+        end
+      end
+    end
+
+    def add_columns(*column_names)
+      list = column_names.collect {|col| col.to_sym }
+      @__column_names__.concat(list)
+      self._define_accessors(column_names)
+      self._define_hooks(@__column_names__)
+    end
+
+    def create_table(table_name, options={}, &block)
+      require 'kwery/table'
+      self.set_table_name(table_name)
+      builder = TableBuilder.new(Query.new(nil))
+      builder.create_table(table_name, options, &block)
+      @__options__ = options
+      @__columns__ = builder.columns
+      self.add_columns(*@__columns__.collect {|c| c._name })
+      @__builder__ = builder
+    end
+
+    def to_sql(query=nil)
+      return nil unless @__builder__
+      return @__builder__.to_sql()
+    end
+
+  end
+
+
   module Model
+
+    def self.included(klass)
+      klass.extend(MetaModel)       # define class methods
+      arr = []
+      klass.name.split('::')[-1].scan(/(?:[A-Z0-9]+[a-z0-9_]+)/) {|s| arr << s.downcase }
+      klass.class_eval do
+        @__table__ = arr.join('_')  # default table name (ex. BlogPost => 'blog_post')
+        @__column_names__ = []
+        @__columns__ = []
+        #extend(MetaModel)
+      end
+      return self
+    end
 
     def [](name)
       return instance_variable_get("@#{name}")
@@ -26,85 +115,6 @@ module Kwery
 
     def __before_update__(values)
       ## empty
-    end
-
-    def self.included(klass)
-      klass.class_eval do
-        arr = []
-        klass.name.split('::')[-1].scan(/(?:[A-Z0-9]+[a-z0-9_]+)/) {|s| arr << s.downcase }
-        @__table__ = arr.join('_')
-        @__column_names__ = []
-        @__columns__ = []
-        class <<self
-          attr_accessor :__table__
-          attr_reader :__column_names__
-          attr_reader :__columns__, :create_table_sql
-        end
-        def self.set_table_name(table_name)
-          @__table__ = table_name
-        end
-        def self._define_accessors(column_names)  # :nodoc:
-          attr_accessor *column_names
-          buf = ''
-          column_names.each do |col|
-            buf << <<-END
-              def #{col}=(val)
-                @__old__[:#{col}] = val if @__old__ && @#{col} != val
-                @#{col} = val
-              end
-            END
-          end
-          self.class_eval(buf)
-        end
-        def self._define_hooks(column_names)  # :nodoc:
-          has_created_at = column_names.include?(:created_at)
-          has_updated_at = column_names.include?(:updated_at)
-          buf = ''
-          if has_created_at && has_updated_at
-            self.class_eval do
-                def __before_insert__(values)
-                  values[:created_at] = @created_at = __current_timestamp__()
-                  values[:updated_at] = @updated_at = __current_timestamp__()
-                end
-            end
-          elsif has_created_at
-            self.class_eval do
-                def __before_insert__(values)
-                  values[:created_at] = @created_at = __current_timestamp__()
-                end
-            end
-          end
-          if has_updated_at
-            self.class_eval do
-                def __before_update__(values)
-                  values[:updated_at] = @updated_at = __current_timestamp__()
-                end
-            end
-          end
-          self.class_eval(buf)
-        end
-        def self.add_columns(*column_names)
-          list = column_names.collect {|col| col.to_sym }
-          @__column_names__ ? (@__column_names__ = list) : @__column_names__.concat(list)
-          self._define_accessors(column_names)
-          self._define_hooks(@__column_names__)
-        end
-        def self.create_table(table_name, options={}, &block)
-          require 'kwery/table'
-          self.set_table_name(table_name)
-          builder = TableBuilder.new(Query.new(nil))
-          builder.create_table(table_name, options, &block)
-          @__options__ = options
-          @__columns__ = builder.columns
-          self.add_columns(*@__columns__.collect {|c| c._name })
-          @__builder__ = builder
-        end
-        def self.to_sql(query=nil)
-          return nil unless @__builder__
-          return @__builder__.to_sql()
-        end
-      end
-      return self
     end
 
     def initialize(*args)
